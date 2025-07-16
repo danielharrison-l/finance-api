3410efa162f8ff7eac83670e8ce4e1db7870de35
José <josepaulo1607@gmail.com>
2025-07-16 09:40:38 -0300

    feat(auth): add required phone field to user registration

--- src/modules/auth/services.ts
+++ src/modules/auth/services.ts
@@ -3,14 +3,12 @@ import { prisma } from '@/lib/prisma'
 import { CreateUserInput, LoginInput } from './schemas'
 
 export async function registerUser(data: CreateUserInput) {
-  const { email, password, name } = data
+  const { email, password, name, phone } = data
 
   const passwordHash = await hash(password, 6)
 
   const userWithSameEmail = await prisma.users.findUnique({
-    where: {
-      email,
-    },
+    where: { email },
   })
 
   if (userWithSameEmail) {
@@ -22,6 +20,7 @@ export async function registerUser(data: CreateUserInput) {
       name,
       email,
       password: passwordHash,
+      phone,
     },
   })
 
@@ -30,6 +29,7 @@ export async function registerUser(data: CreateUserInput) {
       id: user.id,
       name: user.name,
       email: user.email,
+      phone: user.phone,
     },
   }
 }
@@ -38,8 +38,13 @@ export async function authenticateUser(data: LoginInput) {
   const { email, password } = data
 
   const user = await prisma.users.findUnique({
-    where: {
-      email,
+    where: { email },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      password: true,
+      phone: true,
     },
   })
 
@@ -58,6 +63,7 @@ export async function authenticateUser(data: LoginInput) {
       id: user.id,
       name: user.name,
       email: user.email,
+      phone: user.phone,
     },
   }
-} 
+}
